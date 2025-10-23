941a8747563342b65c88eba387278978432f28d7
fezcode <samil.bulbul@gmail.com>
2025-10-24 01:56:25 +0300

    DND: From Serfs and Frauds 6

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { HashRouter as Router } from 'react-router-dom';
 import Layout from './components/Layout';
 import AnimatedRoutes from './components/AnimatedRoutes';
 import { ToastProvider } from './components/ToastProvider';
+import ScrollToTop from './components/ScrollToTop';
 
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <ToastProvider>
         <Layout>
           <AnimatedRoutes />
