4aa287b960c8ec7db8374b303b33c00ed5785cbf
fezcode <samil.bulbul@gmail.com>
2025-11-13 00:50:28 +0300

    fix: toast style

--- src/components/Toast.js
+++ src/components/Toast.js
@@ -16,14 +16,15 @@ const Toast = ({ id, title, message, duration = 3000, removeToast }) => {
   return (
     <motion.div
       initial={{ x: '100%', opacity: 0 }}
-      animate={{ x: 0, opacity: 0.99 }}
+      animate={{ x: 0, opacity: 1 }}
       exit={{ opacity: 0 }}
       transition={{ type: 'spring', stiffness: 120, damping: 20 }}
       className="text-gray-100 font-arvo py-4 px-10 rounded-lg shadow-lg border backdrop-blur-sm flex items-center justify-between w-96 mb-4
        transition-colors bg-toast-background border-toast-border hover:bg-toast-background/90"
     >
-      <div className="flex flex-col text-sm group">
+      <div className="flex flex-col text-sm group w-max flex-grow">
         <span className="text-base text-red-100">{title}</span>
+        <hr className="mt-1 mb-1 min-w-max mr-5 border-red-200" />
         <span className="text-sm text-stone-200">{message}</span>
       </div>
       <button onClick={() => removeToast(id)} className="p-2 border rounded-sm shadow-2xl border-dashed rounded-xs hover:bg-toast-background/100 ">
