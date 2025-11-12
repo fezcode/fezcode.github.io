8663eee77c306c9e819f82c3a0bcd430e4c0b9e2
fezcode <samil.bulbul@gmail.com>
2025-11-12 03:01:44 +0300

    changes due to piml

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -146,7 +146,7 @@ const HomePage = () => {
           <h2 className="text-2xl tracking-tight text-white text-center flex items-center justify-center gap-2 font-arvo">
             <PushPin className="text-primary-400 text-lg" /> Pinned Projects
           </h2>
-          <div className="mt-8 grid grid-cols-1 md:grid-cols-2 gap-8">
+          <div className="mt-8 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
             {pinnedProjects.map((project) => (
               <ProjectCard
                 key={project.slug}
