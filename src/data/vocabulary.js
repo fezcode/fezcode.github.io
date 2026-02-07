bcb1fc11408a85300f27858686c5c8f45496c20f
fezcode <samil.bulbul@gmail.com>
2026-02-07 06:12:33 +0300

    content(philosophy): philosophy-101

--- src/data/vocabulary.js
+++ src/data/vocabulary.js
@@ -1,106 +1,186 @@
 export const vocabulary = {
+  absurdism: {
+    title: 'Absurdism',
+    loader: () => import('./vocab/absurdism'),
+  },
   antikythera: {
     title: 'Antikythera Mechanism',
     loader: () => import('./vocab/antikythera'),
-  },
-  'prop-drilling': {
-    title: 'Prop Drilling',
-    loader: () => import('./vocab/prop-drilling'),
-  },
-  'context-api': {
-    title: 'Context API',
-    loader: () => import('./vocab/context-api'),
   },
   'anti-pattern': {
     title: 'Anti-Pattern',
     loader: () => import('./vocab/anti-pattern'),
   },
-  pluribus: {
-    title: 'Pluribus',
-    loader: () => import('./vocab/pluribus'),
+  'box-plot': {
+    title: 'Box Plot',
+    loader: () => import('./vocab/box-plot'),
   },
-  'side-effects': {
-    title: 'Side Effects',
-    loader: () => import('./vocab/side-effects'),
+  'cogito-ergo-sum': {
+    title: 'Cogito, Ergo Sum',
+    loader: () => import('./vocab/cogito-ergo-sum'),
   },
-  'poi-rating': {
-    title: "Person of Interest's Rating",
-    loader: () => import('./vocab/poi-rating'),
-  },
-  'game-of-the-year': {
-    title: 'Game of the Year (GOTY)',
-    loader: () => import('./vocab/game-of-the-year'),
-  },
-  murakami: {
-    title: 'Murakami, Takashi',
-    loader: () => import('./vocab/murakami'),
-  },
-  ssg: {
-    title: 'Static Site Generation (SSG)',
-    loader: () => import('./vocab/ssg'),
-  },
-  csr: {
-    title: 'Client-Side Rendering (CSR)',
-    loader: () => import('./vocab/csr'),
-  },
-  hydration: {
-    title: 'Hydration',
-    loader: () => import('./vocab/hydration'),
-  },
-  spa: {
-    title: 'Single Page Application (SPA)',
-    loader: () => import('./vocab/spa'),
+  'context-api': {
+    title: 'Context API',
+    loader: () => import('./vocab/context-api'),
   },
   crawler: {
     title: 'Search Engine Crawler',
     loader: () => import('./vocab/crawler'),
   },
-  'open-graph': {
-    title: 'Open Graph Protocol',
-    loader: () => import('./vocab/open-graph'),
+  csr: {
+    title: 'Client-Side Rendering (CSR)',
+    loader: () => import('./vocab/csr'),
   },
-  'name-mangling': {
-    title: 'Name Mangling',
-    loader: () => import('./vocab/name-mangling'),
+  dasein: {
+    title: 'Dasein',
+    loader: () => import('./vocab/dasein'),
   },
-  'modules-vs-includes': {
-    title: 'Modules vs. Includes',
-    loader: () => import('./vocab/modules-vs-includes'),
+  dialectic: {
+    title: 'Dialectic (Hegelian)',
+    loader: () => import('./vocab/dialectic'),
   },
-  'lingua-franca': {
-    title: 'Lingua Franca',
-    loader: () => import('./vocab/lingua-franca'),
-  },
-  'power-law': {
-    title: 'Power Law (Long-tail)',
-    loader: () => import('./vocab/power-law'),
-  },
-  'box-plot': {
-    title: 'Box Plot',
-    loader: () => import('./vocab/box-plot'),
-  },
-  p99: {
-    title: 'P99 (99th Percentile)',
-    loader: () => import('./vocab/p99'),
+  epistemology: {
+    title: 'Epistemology',
+    loader: () => import('./vocab/epistemology'),
   },
   etl: {
     title: 'ETL (Extract, Transform, Load)',
     loader: () => import('./vocab/etl'),
   },
-  staccato: {
-    title: 'Staccato (Music)',
-    loader: () => import('./vocab/staccato'),
+  existentialism: {
+    title: 'Existentialism',
+    loader: () => import('./vocab/existentialism'),
+  },
+  'game-of-the-year': {
+    title: 'Game of the Year (GOTY)',
+    loader: () => import('./vocab/game-of-the-year'),
+  },
+  geist: {
+    title: 'Geist',
+    loader: () => import('./vocab/geist'),
+  },
+  hydration: {
+    title: 'Hydration',
+    loader: () => import('./vocab/hydration'),
+  },
+  'language-games': {
+    title: 'Language Games',
+    loader: () => import('./vocab/language-games'),
+  },
+  'leap-of-faith': {
+    title: 'Leap of Faith',
+    loader: () => import('./vocab/leap-of-faith'),
   },
   legato: {
     title: 'Legato (Music)',
     loader: () => import('./vocab/legato'),
   },
+  'lingua-franca': {
+    title: 'Lingua Franca',
+    loader: () => import('./vocab/lingua-franca'),
+  },
+  'modules-vs-includes': {
+    title: 'Modules vs. Includes',
+    loader: () => import('./vocab/modules-vs-includes'),
+  },
+  murakami: {
+    title: 'Murakami, Takashi',
+    loader: () => import('./vocab/murakami'),
+  },
+  'name-mangling': {
+    title: 'Name Mangling',
+    loader: () => import('./vocab/name-mangling'),
+  },
+  nihilism: {
+    title: 'Nihilism',
+    loader: () => import('./vocab/nihilism'),
+  },
+  occasionalism: {
+    title: 'Occasionalism',
+    loader: () => import('./vocab/occasionalism'),
+  },
+  ontology: {
+    title: 'Ontology',
+    loader: () => import('./vocab/ontology'),
+  },
+  'open-graph': {
+    title: 'Open Graph Protocol',
+    loader: () => import('./vocab/open-graph'),
+  },
+  p99: {
+    title: 'P99 (99th Percentile)',
+    loader: () => import('./vocab/p99'),
+  },
+  pluribus: {
+    title: 'Pluribus',
+    loader: () => import('./vocab/pluribus'),
+  },
+  'poi-rating': {
+    title: "Person of Interest's Rating",
+    loader: () => import('./vocab/poi-rating'),
+  },
+  'power-law': {
+    title: 'Power Law (Long-tail)',
+    loader: () => import('./vocab/power-law'),
+  },
+  'prop-drilling': {
+    title: 'Prop Drilling',
+    loader: () => import('./vocab/prop-drilling'),
+  },
+  qualia: {
+    title: 'Qualia',
+    loader: () => import('./vocab/qualia'),
+  },
+  'side-effects': {
+    title: 'Side Effects',
+    loader: () => import('./vocab/side-effects'),
+  },
+  'socratic-method': {
+    title: 'Socratic Method',
+    loader: () => import('./vocab/socratic-method'),
+  },
+  solipsism: {
+    title: 'Solipsism',
+    loader: () => import('./vocab/solipsism'),
+  },
+  spa: {
+    title: 'Single Page Application (SPA)',
+    loader: () => import('./vocab/spa'),
+  },
+  ssg: {
+    title: 'Static Site Generation (SSG)',
+    loader: () => import('./vocab/ssg'),
+  },
+  staccato: {
+    title: 'Staccato (Music)',
+    loader: () => import('./vocab/staccato'),
+  },
+  'theory-of-forms': {
+    title: 'Theory of Forms',
+    loader: () => import('./vocab/theory-of-forms'),
+  },
+  'transcendental-idealism': {
+    title: 'Transcendental Idealism',
+    loader: () => import('./vocab/transcendental-idealism'),
+  },
   tremolo: {
     title: 'Tremolo (Music)',
     loader: () => import('./vocab/tremolo'),
   },
+  ubermensch: {
+    title: 'Übermensch',
+    loader: () => import('./vocab/ubermensch'),
+  },
+  utilitarianism: {
+    title: 'Utilitarianism',
+    loader: () => import('./vocab/utilitarianism'),
+  },
   vibrato: {
     title: 'Vibrato (Music)',
     loader: () => import('./vocab/vibrato'),
+  },
+  'will-to-live': {
+    title: 'Will to Live',
+    loader: () => import('./vocab/will-to-live'),
   },
 };
