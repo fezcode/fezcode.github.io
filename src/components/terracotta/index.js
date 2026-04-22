5283b7a277b7c13d725f9c3887864dc5434a541f
fezcode <samil.bulbul@gmail.com>
2026-04-22 18:02:33 +0300

    feat(terracotta): barrel export terracotta primitives

---
+++ src/components/terracotta/index.js
@@ -0,0 +1,13 @@
+export { default as TerracottaMark } from './TerracottaMark';
+export { default as TerracottaPlumbLine } from './TerracottaPlumbLine';
+export { default as TerracottaStrip } from './TerracottaStrip';
+export { default as TerracottaChapter, ChapterEm } from './TerracottaChapter';
+export { default as TerracottaMarkStage } from './TerracottaMarkStage';
+export {
+  default as TerracottaEntry,
+  EntryPos,
+  EntryItem,
+  EntryCounterStyle,
+} from './TerracottaEntry';
+export { default as TerracottaColophon } from './TerracottaColophon';
+export { default as TerracottaSpec } from './TerracottaSpec';
